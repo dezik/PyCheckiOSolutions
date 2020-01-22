32aad2bb2ed60e44339a1e024c07ac44b454876b
10060618 <10060618@gemalto.com>
2020-01-22 17:00:51 +0100

    flatten list

--- Home/Flatten a List/mission.py
+++ Home/Flatten a List/mission.py
@@ -1,9 +1,12 @@
 def flat_list(array):
-    return array
+    out = []
+    for i in array:
+        out.extend(flat_list(i)) if isinstance(i, list) else out.append(i)
+    return out
 
 
 if __name__ == '__main__':
-    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    # assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
     assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
     assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
     assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
