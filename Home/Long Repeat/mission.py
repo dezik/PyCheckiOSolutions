902680883185981e8819bb93b638f498507c0b48
10060618 <10060618@gemalto.com>
2020-01-23 11:22:38 +0100

    long repeat

--- Home/Long Repeat/mission.py
+++ Home/Long Repeat/mission.py
@@ -1,14 +1,18 @@
+import re
+
+
 def long_repeat(line: str) -> int:
-    """
-        length the longest substring that consists of the same char
-    """
-    # your code here
-    return 0
+    if len(line) < 2:
+        return len(line)
+    max_len = 1
+    for m in re.finditer(r"(\w)\1+", line):
+        max_len = max(len(m.group()), max_len)
+    return max_len
 
 
 if __name__ == '__main__':
     # These "asserts" using only for self-checking and not necessary for auto-testing
-    assert long_repeat('sdsffffse') == 4, "First"
+    assert long_repeat('abababa') == 1, "First"
     assert long_repeat('ddvvrwwwrggg') == 3, "Second"
     assert long_repeat('abababaab') == 2, "Third"
     assert long_repeat('') == 0, "Empty"
