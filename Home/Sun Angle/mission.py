8e678f677cf6c39b1f8ffc2f0b66a4c763f83d8c
10060618 <10060618@gemalto.com>
2020-01-23 13:43:01 +0100

    sun angle

--- Home/Sun Angle/mission.py
+++ Home/Sun Angle/mission.py
@@ -1,13 +1,12 @@
-def sun_angle(time):
-    # replace this for solution
-    return time
+def sun_angle(time: str):
+    hours, minutes = map(lambda x: int(x), time.split(":"))
+    minutes = (hours - 6) * 60 + minutes
+    if minutes < 0 or minutes > 720:
+        return "I don't see the sun!"
+    return round(minutes / 4, 2)
 
 
 if __name__ == '__main__':
-    print("Example:")
-    print(sun_angle("07:00"))
-
-    # These "asserts" using only for self-checking and not necessary for auto-testing
     assert sun_angle("07:00") == 15
     assert sun_angle("01:23") == "I don't see the sun!"
     print("Coding complete? Click 'Check' to earn cool rewards!")
