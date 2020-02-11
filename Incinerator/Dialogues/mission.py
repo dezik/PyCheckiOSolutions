ef7e1cf0fbca18ec4982b6836c1b3aeec9fb1e95
10060618 <10060618@gemalto.com>
2020-02-11 16:56:47 +0100

    dialogues

--- Incinerator/Dialogues/mission.py
+++ Incinerator/Dialogues/mission.py
@@ -1,17 +1,50 @@
 VOWELS = "aeiou"
 
+
+class User:
+    def __init__(self, name: str):
+        self.name = name
+
+    def send(self, s: str):
+        self._dialogue.append(self.name + " said: " + s)
+
+    def _add_to_chat(self, dialogue: list):
+        self._dialogue = dialogue
+
+
+class Human(User):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+
+class Robot(User):
+    def __init__(self, serial_number: str):
+        super().__init__(serial_number)
+
+
 class Chat:
-    pass
+    def __init__(self):
+        self.dialogue = []
 
-class Human:
-    pass
+    def connect_human(self, human: Human):
+        human._add_to_chat(self.dialogue)
 
-class Robot:
-    pass
+    def connect_robot(self, robot: Robot):
+        robot._add_to_chat(self.dialogue)
+
+    def show_human_dialogue(self):
+        return "\n".join(self.dialogue)
+
+    def show_robot_dialogue(self):
+        robo_dialog = []
+        for line in self.dialogue:
+            parts = line.split(": ")
+            robo_dialog.append(parts[0] + ": " + "".join(["0" if x in VOWELS else "1" for x in parts[1]]))
+        return "\n".join(robo_dialog)
 
 
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    # These "asserts" using only for self-checking and not necessary for auto-testing
 
     chat = Chat()
     karl = Human("Karl")
---
+++ PyCon TW/Square Spiral/mission.py
@@ -0,0 +1,11 @@
+def find_distance(first, second):
+    return 1
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert find_distance(1, 9) == 2, "First"
+    assert find_distance(9, 1) == 2, "Reverse First"
+    assert find_distance(10, 25) == 1, "Neighbours"
+    assert find_distance(5, 9) == 4, "Diagonal"
+    assert find_distance(26, 31) == 5, "One row"
+    assert find_distance(50, 16) == 10, "One more test"
